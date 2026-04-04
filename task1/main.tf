bedae9c16885ca75d45d5f1cef02b100157d353b
Anastasia Paslavska <a.paslavska2206@gmail.com>
2026-04-04 22:01:17 +0300

    update task3

--- task1/main.tf
+++ task1/main.tf
@@ -1,17 +1,8 @@
 terraform {
   required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "4.0.4"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.4.0"
-    }
+    digitalocean = { source = "digitalocean/digitalocean", version = "~> 2.0" }
+    tls          = { source = "hashicorp/tls", version = "4.0.4" }
+    local        = { source = "hashicorp/local", version = "2.4.0" }
   }
   backend "s3" {
     endpoint                    = "https://fra1.digitaloceanspaces.com"
@@ -26,12 +17,9 @@ terraform {
 }
 
 variable "do_token" {}
+provider "digitalocean" { token = var.do_token }
 
-provider "digitalocean" {
-  token = var.do_token
-}
-
-# --- 1. SSH KEY GENERATION ---
+# --- 1. SSH KEY ---
 resource "tls_private_key" "paslavska_ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -42,7 +30,6 @@ resource "digitalocean_ssh_key" "paslavska_key" {
   public_key = tls_private_key.paslavska_ssh_key.public_key_openssh
 }
 
-# Збереження приватного ключа для Ansible
 resource "local_file" "paslavska_private_key" {
   content         = tls_private_key.paslavska_ssh_key.private_key_pem
   filename        = "${path.module}/id_rsa.pem"
@@ -50,20 +37,17 @@ resource "local_file" "paslavska_private_key" {
 }
 
 # --- 2. VPC ---
-data "digitalocean_vpc" "existing_vpc" {
-  name = "paslavska-vpc"
-}
+data "digitalocean_vpc" "existing_vpc" { name = "paslavska-vpc" }
 
 # --- 3. DROPLET ---
 resource "digitalocean_droplet" "paslavska_node" {
   name     = "paslavska-node"
   region   = "fra1"
-  size     = "s-4vcpu-8gb"
+  size     = "s-4vcpu-8gb" # 8GB для стабільності K8s
   image    = "ubuntu-24-04-x64"
   vpc_uuid = data.digitalocean_vpc.existing_vpc.id
   ssh_keys = [digitalocean_ssh_key.paslavska_key.id]
 
-  # Динамічне створення inventory.ini
   provisioner "local-exec" {
     command = "echo '[paslavska_nodes]\n${self.ipv4_address} ansible_user=root ansible_ssh_private_key_file=task1/id_rsa.pem ansible_ssh_common_args=\"-o StrictHostKeyChecking=no\"' > ../task2/inventory.ini"
   }
@@ -73,7 +57,6 @@ resource "digitalocean_droplet" "paslavska_node" {
 resource "digitalocean_firewall" "paslavska_firewall" {
   name        = "paslavska-firewall"
   droplet_ids = [digitalocean_droplet.paslavska_node.id]
-
   inbound_rule {
     protocol         = "tcp"
     port_range       = "22"
@@ -81,7 +64,7 @@ resource "digitalocean_firewall" "paslavska_firewall" {
   }
   inbound_rule {
     protocol         = "tcp"
-    port_range       = "8000-8003"
+    port_range       = "8000-8003" # Відкриваємо порти для застосунку
     source_addresses = ["0.0.0.0/0"]
   }
   outbound_rule {
@@ -93,4 +76,9 @@ resource "digitalocean_firewall" "paslavska_firewall" {
 
 output "droplet_ip" {
   value = digitalocean_droplet.paslavska_node.ipv4_address
+}
+
+output "private_key" {
+  value     = tls_private_key.paslavska_ssh_key.private_key_pem
+  sensitive = true
 }
